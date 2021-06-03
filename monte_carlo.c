6246efdf79bb89273f97c8276fce738b866ad15e
varsha2801 <84717141+varsha2801@users.noreply.github.com>
2021-06-03 20:43:06 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -39,5 +39,16 @@ int main(void) {
   }
 }
 
-
+  float mc_pi(int n)
+  {
+   int circle=0;
+   float x,y;
+   for(int i=1;i<=n;i++)
+   {
+      x=frandom();
+      y+frandom();
+     if(x*x + y*y <= 1){circle++;}
+   }
+    return 4*((float)circle/n);
+  }
 
